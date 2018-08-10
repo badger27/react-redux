ca416829cd522615d68a75bc35b32d84ab38ebbe
badger27 <s.freir@gmail.com>
2018-08-10 16:24:34 +0100

    fixed styles

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,18 +1,17 @@
 import React from 'react';
+import  {Link} from 'react-router';
 
 
 class AboutPage extends React.Component{
   render(){
     return (
-
     <div className = "jumbotron">
       <h1>About</h1>
-      <p>react</p>
+      <p>React</p>
       <Link to="about" className="btn btn-primary btn-lg" >Learn more</Link>
     </div>
     )
   }
-
 }
 
 export default AboutPage;
